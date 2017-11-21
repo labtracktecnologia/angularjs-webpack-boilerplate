c1c11b01b334ee736e20a2b15f7a01541329ee41
Jhony Alceu Pereira <jhonystein@gmail.com>
2017-11-21 21:04:14 -0200

    exemplos de codigo

--- src/app/clientes/form.controller.js
+++ src/app/clientes/form.controller.js
@@ -1,1 +1,30 @@
-export default class FormController {}
+export default class FormController {
+
+    constructor($stateParams, $http, $state) {
+        this.record = {};
+        this.title = 'Adicionando registro';
+        if ($stateParams.id) {
+            this.title = 'Editando registro';
+            $http.get(`http://localhost:8080/api/clientes/${$stateParams.id}`)
+              .then((response) => {
+                  this.record = response.data;
+              });
+        }
+        this._http = $http;
+        this._state = $state;
+    }
+
+    save() {
+        let httpReq;
+        if (this.record._id) {
+            httpReq = this._http.put(`http://localhost:8080/api/clientes/${this.record._id}`, this.record);
+        } else {
+            httpReq = this._http.post(`http://localhost:8080/api/clientes`, this.record);
+        }
+        httpReq.then(resp => {
+            this._state.go('cliente.list')
+        })
+    }
+}
+
+FormController.$inject = ['$stateParams', '$http', '$state'];
--- src/app/clientes/list.controller.js
+++ src/app/clientes/list.controller.js
@@ -1,1 +1,18 @@
-export default class ListController {}
+export default class ListController {
+
+    constructor($http) {
+        this.filter = '';
+        this.records = [];
+        this._http = $http;
+        this.load();
+    }
+
+    load() {
+        this._http.get('http://localhost:8080/api/clientes').
+          then((response) => {
+              this.records = response.data;
+          });
+    }
+}
+
+ListController.$inject = ['$http']
